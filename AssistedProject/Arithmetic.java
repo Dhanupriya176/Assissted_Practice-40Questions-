a5f8e3bf4134eba8f707d8dabf5d88d89a3286c1
B Dhanu Priya <dhanupriya873@gmail.com>
2022-01-16 21:20:28 +0530

    my project

---
+++ AssistedProject/Arithmetic.java
@@ -0,0 +1,31 @@
+package AssistedProject;
+
+import java.util.Scanner;
+	
+	public class Arithmetic {
+		
+			public static void main(String[] args) {
+				
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter the two numbers");
+				int Num1=sc.nextInt();
+				int Num2= sc.nextInt();
+				System.out.println("Enter the operator ");
+				char op=sc.next().charAt(0);
+				double Ans=0;
+
+				switch(op){
+				case '+': Ans=Num1+Num2;
+				break;
+				case '-':Ans=Num1-Num2;
+				break;
+				case '*': Ans=Num1*Num2;
+				break;
+				case '/':Ans=Num1/Num2;
+				break;
+				}
+				System.out.println("The Answer is " +Ans);
+			}
+		}
+
+
